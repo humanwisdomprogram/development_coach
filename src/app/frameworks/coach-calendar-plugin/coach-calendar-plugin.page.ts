ebc3e1248bdf8eaca1577c7ecdd414a54160b2ed
PavishJain21 <jainpavish@gmail.com>
2022-01-22 17:53:06 +0530

    Done with Upcoming appointment

--- src/app/frameworks/coach-calendar-plugin/coach-calendar-plugin.page.ts
+++ src/app/frameworks/coach-calendar-plugin/coach-calendar-plugin.page.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
-import { NgbCalendar, NgbDate, NgbDateParserFormatter, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCalendar, NgbDate, NgbTimeStruct } from '@ng-bootstrap/ng-bootstrap';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { DataService } from '../services/data.service';
+import * as moment from 'moment';
+
 declare var $: any;
 @Component({
   selector: 'app-coach-calendar-plugin',
@@ -32,49 +34,73 @@ declare var $: any;
 export class CoachCalendarPluginPage implements OnInit {
   public calenderPlugin: FormGroup;
   hoveredDate: NgbDate | null = null;
-  startTime: NgbTimeStruct = {hour: 13, minute: 30, second: 0};
-  endTime: NgbTimeStruct = {hour: 13, minute: 30, second: 0};
+  startTime: NgbTimeStruct = { hour: 13, minute: 30, second: 0 };
+  endTime: NgbTimeStruct = { hour: 13, minute: 30, second: 0 };
   dropdownSettings: IDropdownSettings = {};
   languageList = [];
   hourStep = 1;
   minuteStep = 15;
   secondStep = 30
   fromDate: NgbDate;
+  nearestDate = null;
+  appointmentDates = '';
   toDate: NgbDate | null = null;
 
-  constructor(calendar: NgbCalendar,  private dataservice: DataService, private formbuilder: FormBuilder) {
-    
+  constructor(calendar: NgbCalendar, private dataservice: DataService, private formbuilder: FormBuilder) {
+
     this.fromDate = calendar.getToday();
     this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
     this.languageList = this.dataservice.getDayes().
-    map(x => new Object({ item_id: x.name, item_text: x.name }));
-  this.dropdownSettings = {
-    idField: 'item_id',
-    textField: 'item_text',
-  };
+      map(x => new Object({ item_id: x.name, item_text: x.name }));
+    this.dropdownSettings = {
+      idField: 'item_id',
+      textField: 'item_text',
+    };
   }
 
   ngOnInit() {
-  
 
-    $('#time').datetimepicker({  
-      format: 'HH:mm'  
-  });  
-  this.calenderPlugin = this.formbuilder.group({
-    DateRange: [''],
-    StartTime: [''],
-    EndTime: [''],
-    SelectedDayes: ['']
-  });
+    this.onGetDates();
+    $('#time').datetimepicker({
+      format: 'HH:mm'
+    });
+    this.calenderPlugin = this.formbuilder.group({
+      DateRange: [''],
+      StartTime: [''],
+      EndTime: [''],
+      SelectedDayes: ['']
+    });
   }
- 
-onAdd() {
-  // (this.calenderPlugin as FormArray).push(this.formbuilder.group({
-  //   DateRange: [''],
-  //   StartTime: [''],
-  //   EndTime: [''],
-  //   SelectedDayes: ['']
-  // }))
+
+  onGetDates() {
+    const getData = this.dataservice.getData();
+    if (getData.length > 0) {
+      getData.map(res => {
+        res['BookingDates'] = res.Date.split(' ')[0].split('/')[2] + '-' + res.Date.split(' ')[0].split('/')[1] + '-' + res.Date.split(' ')[0].split('/')[0] + ' ' + moment(res.StartTime.split(' ')[1] + res.StartTime.split(' ')[2], 'hh:mm A').format('HH:mm');
+      });
+    }
+
+    const date = new Date();
+    const n = date.toDateString();
+    const time = date.toLocaleTimeString();
+
+    const dateToCheckFor = moment(n).format('YYYY-MM-DD') + ' ' + moment(time, 'hh:mm A').format('HH:mm');
+
+    getData.forEach(date => {
+      let diff = moment(date['BookingDates']).diff(moment(dateToCheckFor), 'minutes');
+      if (diff > 0) {
+        if (this.nearestDate) {
+          if (moment(date['BookingDates']).diff(moment(this.nearestDate?.BookingDates), 'minutes') < 0) {
+            this.nearestDate = date;
+          }
+        } else {
+          this.nearestDate = date;
+        }
+      }
+    });
+
+    this.appointmentDates = moment(this.nearestDate?.BookingDates).format('dddd, D MMMM YYYY, hh A')
+    // + ' - ' +  moment(this.nearestDate?.EndTime.split(' ')[1]+this.nearestDate?.EndTime.split(' ')[2], 'hh:mm A').format('HH:mm A');
   }
 
 
